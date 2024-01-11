260eca3df674afd7d39ffc5f1bc5971ad65868b8
Manuel Maier <maiermanuel423@gmail.com>
2024-01-11 13:21:19 +0100

    Mein Projekt hinzugefügt, ich habe momentan Probleme

---
+++ Mein Projekt/fall_modul/src/main/java/at/spengergasse/med_px/Med4bhbgmApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.med_px;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Med4bhbgmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Med4bhbgmApplication.class, args);
+    }
+
+}
---
+++ Mein Projekt/fall_modul/src/test/java/at/spengergasse/med_px/Med4ahbgmApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.med_px;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Med4ahbgmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
