66afe21f3034d5f0ea11d33252903be114f7c038
Manuel Maier <maiermanuel423@gmail.com>
2024-01-18 12:30:00 +0100

    ERste Teile meiner App (richtig) hinzugefügt. Next TODO: WebMvcConfiguer einbauen

--- src/main/java/at/spengergasse/med_px/entities/Doctor.java
+++ src/main/java/at/spengergasse/med_px/entities/Doctor.java
@@ -1,7 +1,16 @@
 package at.spengergasse.med_px.entities;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Doctor extends Staff{
+    private String name;
 }
--- src/main/java/at/spengergasse/med_px/entities/Patient.java
+++ src/main/java/at/spengergasse/med_px/entities/Patient.java
@@ -1,9 +1,17 @@
 package at.spengergasse.med_px.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 public class Patient {
     @Id
@@ -19,4 +27,23 @@ public class Patient {
     @ManyToOne
     @JoinColumn(name = "insurance_id")
     private Insurance insurance;
+
+
+
+    @Column(name = "p_name")
+    private String name;
+
+    @OneToMany(mappedBy = "patient")
+    private List<MedicalCase> faelle;
+
+    public void addFall(MedicalCase f){
+        this.faelle.add(f);
+    }
+
+    @Override
+    public String toString() {
+        return name + ", Fälle:" + faelle;
+    }
+
+
 }
